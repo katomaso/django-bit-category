eb96e1e01358e34126c30cbac0bc84a6e6ec4429
Tomas Peterka <prestizni@gmail.com>
2016-08-17 22:29:44 +0200

    Remove django.conf.urls.pattern to work with django 1.10+

--- bitcategory/urls.py
+++ bitcategory/urls.py
@@ -1,22 +1,34 @@
 # coding: utf-8
-'''
-Please include these !without namespace! and with your custom model.
+"""Please include these !without namespace! and with your custom model.
+
 The views included here usually serves for AJAX requests for forms.
 
 The usual way is to put::
 
     from myapp.models import MyCategory
 
-    urlpatterns = patterns('',
+    urlpatterns = [
         url(r'', include('bitcategory.urls'), {"model": MyCategory})
-    )
+    ]
 
 By using a namespace you can use more hierarchical models. Then you need to
 pass the namespace into a HierarchicalField associated with the custom model.
-'''
-from django.conf.urls import patterns, url
+"""
+import six
+
+from django.conf.urls import url
 from bitcategory.views import ajax
 
-urlpatterns = [
+
+def patterns(*args):
+    try:
+        from django.conf.urls import patterns
+        if isinstance(args[0], (six.text_type, six.binary_type)):
+            return patterns(*args)
+        return patterns('', *args)
+    except ImportError:
+        return args
+
+urlpatterns = patterns(
     url(r'^hierarchical_ajax$', ajax, name="hierarchical_ajax"),
-]
+)
--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -1,10 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from bitcategory.models import Category
 
 # admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+
+def _patterns(*args):
+    try:
+        from django.conf.urls import patterns
+        return patterns('', *args)
+    except ImportError:
+        return args
+
+urlpatterns = _patterns(
     url(r'^$', 'testapp.views.index', name="index"),
     url('', include('bitcategory.urls'), {"model": Category}),
     # url(r'^admin/', include(admin.site.urls)),
